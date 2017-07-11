de0f88567bbba295e412b18dbaf336c5fcfcf593
Mingjie Li <limingjie@users.noreply.github.com>
2017-07-12 00:26:27 +0800

    Add comments

--- where.cpp
+++ where.cpp
@@ -8,6 +8,7 @@ typedef std::vector<std::string> row_t;
 typedef std::vector<row_t> table_t;
 typedef int operator_t;
 
+// Base Condition class, to make sure all types of conditions can be invoked using the same base type.
 class ConditionBase
 {
 public:
@@ -18,10 +19,13 @@ public:
     static const operator_t GT = 4;
     static const operator_t GE = 5;
 
+    // A table class should be defined to encapsulate table header and table rows,
+    // then the function parameter could be (table, row_index).
     virtual bool eval(header_t &header, row_t &row) = 0;
     virtual void print() = 0;
 };
 
+// Subclass for any type of conditions.
 template <typename T>
 class Condition: public ConditionBase
 {
@@ -29,7 +33,9 @@ private:
     std::string column;
     operator_t op;
     T value;
+
 public:
+    // construct a new condition. i.e. name = "John Doe"
     Condition(std::string column, operator_t op, T value)
     {
         this->column = column;
@@ -46,6 +52,7 @@ public:
     }
 };
 
+// Handle integer
 template <>
 bool Condition<int>::eval(header_t &header, row_t &row)
 {
@@ -62,6 +69,7 @@ bool Condition<int>::eval(header_t &header, row_t &row)
     }
 }
 
+// Handle floating point number
 template <>
 bool Condition<float>::eval(header_t &header, row_t &row)
 {
@@ -78,6 +86,7 @@ bool Condition<float>::eval(header_t &header, row_t &row)
     }
 }
 
+// Handle string
 template <>
 bool Condition<std::string>::eval(header_t &header, row_t &row)
 {
@@ -94,11 +103,13 @@ bool Condition<std::string>::eval(header_t &header, row_t &row)
     }
 }
 
+// Where Clause
 class Where
 {
 private:
-    std::vector<ConditionBase *> conditions;
-    std::vector<operator_t> operators;
+    std::vector<ConditionBase *> conditions; // all conditions in the clause
+    std::vector<operator_t> operators;       // all operators in the clause
+
 public:
     static const operator_t AND = 0;
     static const operator_t OR  = 1;
@@ -115,29 +126,33 @@ public:
         return this;
     }
 
-    bool eval(header_t &header, row_t &row, int index = 0)
+    // - A table class should be defined to encapsulate table header and table rows,
+    // then the function parameter could be (table, row_index, op_index).
+    // - The op_index is the start operator for evaluation, the function is written
+    // recursively to simplify the code.
+    bool eval(header_t &header, row_t &row, int op_index = 0)
     {
-        bool result = conditions[index]->eval(header, row);
+        bool result = conditions[op_index]->eval(header, row);
 
         // debug
-        // conditions[index]->print();
+        // conditions[op_index]->print();
         // std::cout << " -> " << (result ? "true" : "false") << std::endl;
 
-        while(index < operators.size())
+        while(op_index < operators.size())
         {
-            if (operators[index] == AND)
+            if (operators[op_index] == AND)
             {
-                index++;
-                result = result && conditions[index]->eval(header, row);
+                op_index++;
+                result = result && conditions[op_index]->eval(header, row);
 
                 // debug
-                // conditions[index]->print();
-                // std::cout << " -> " << (conditions[index]->eval(header, row) ? "true" : "false") << std::endl;
+                // conditions[op_index]->print();
+                // std::cout << " -> " << (conditions[op_index]->eval(header, row) ? "true" : "false") << std::endl;
             }
             else // OR
             {
-                index++;
-                result = result || eval(header, row, index);
+                op_index++;
+                result = result || eval(header, row, op_index);
                 break; // break here, thus this function can be written as non-recursive.
             }
         }
@@ -150,11 +165,11 @@ int main()
 {
     header_t header = {{"name", 0}, {"age", 1}, {"gender", 2}, {"score", 3}, {"company", 4}};
     table_t table = {
-        {"John Doe", "20", "male", "110.5", "IBX"},
-        {"Jenny Ho", "21", "female", "100", "Huawei"},
-        {"Bill Gates", "61", "male", "101", "Microsoft"},
-        {"Paul Allen", "64", "male", "102", "Microsoft"},
-        {"Jan Doe", "32", "female", "199", "Microsoft"}
+        {"John Doe",   "20", "male",   "110.5", "IBX"      },
+        {"Jenny Ho",   "21", "female", "100",   "Huawei"   },
+        {"Bill Gates", "61", "male",   "101",   "Microsoft"},
+        {"Paul Allen", "64", "male",   "102",   "Microsoft"},
+        {"Jane Doe",   "32", "female", "199",   "Microsoft"}
     };
 
     // WHERE name != "Bill Gates" AND age > 60 OR gender = "female" AND score <= 100 OR company = "IBX"
@@ -169,6 +184,8 @@ int main()
      ->AddOperator(Where::OR)
      ->AddCondition(new Condition<std::string>("company", ConditionBase::EQ, "IBX"));
 
+    std::cout << "name\t\tage\tgender\tscore\tcompany\n"
+              << "---------+---------+---------+---------+---------+\n";
     for (int i = 0; i < table.size(); i++)
     {
         if (w->eval(header, table[i]))
